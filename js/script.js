fea8b50281fba903c7a2fb5bc1150a6da9cc48af
ShinyUnicorn <70696290+JessicaSmits@users.noreply.github.com>
2021-08-27 13:37:39 +0200

    Final upload - responsive done!

--- js/script.js
+++ js/script.js
@@ -1,19 +1,45 @@
-var mediaQuery = window.matchMedia("(min-width: 1025px)")
-$(document).ready(removeClear());
+var desktopSize = window.matchMedia("(min-width:1026px");
+var tabletSize = window.matchMedia("(max-width: 1025px)");
+var checkTablet = window.matchMedia("(min-width: 720px)")
+var mobileSize = window.matchMedia("(max-width: 719px)");
+$(document).ready(windowSize());
+$("div.burger-icon").click(burgerMenu);
+console.log(tabletSize);
 
-function removeClear() {
-    if (mediaQuery.matches) {
-        divclear = $("#removable-clear");
-        divclear.removeClass("clear");
-        $("#languages h3").addClass("bottom");
-        
-
-    } else {
+function windowSize() {
+    if (tabletSize.matches && checkTablet.matches) {
         divclear = $("#removable-clear");
         divclear.addClass("clear");
 
         $("#languages h3").removeClass("bottom");
+
+    } else if (mobileSize.matches || desktopSize.matches) {
+        divclear = $("#removable-clear");
+        divclear.removeClass("clear");
+        $("#languages h3").addClass("bottom");
     }
+
+    if (mobileSize.matches) {
+        $("nav.standard").hide();
+        $("nav.burger-menu, div.burger-icon").show();
+        $("section.contact div.links h3").hide()
+
+    } else {
+        $("nav.standard").show();
+        $("nav.burger-menu, div.burger-icon").hide();
+        $("section.contact div.links h3").show()
+    }
+
+    widthOfImg = $(".project img").width()
+    $("p.photo-text").css({"max-width": widthOfImg})
 }
 
-window.onresize = removeClear;
+function burgerMenu(){
+    $("div.overlay").animate({
+        "width": "toggle"
+    }, 300);
+    $("div.burger-icon, div.bar, #bar2, #bar1, #bar3, body").toggleClass("open");
+
+}
+
+window.onresize = windowSize;
