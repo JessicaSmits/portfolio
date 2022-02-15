3afbed7f1065a99d01cd326a383a00a32e387ead
ShinyUnicorn <70696290+JessicaSmits@users.noreply.github.com>
2022-02-15 21:50:33 +0100

    aestetic/ux update
    also some issue fixes

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var desktopSize = window.matchMedia("(min-width:1026px");
 var tabletSize = window.matchMedia("(max-width: 1025px)");
 var checkTablet = window.matchMedia("(min-width: 720px)")
 var mobileSize = window.matchMedia("(max-width: 719px)");
+
 $(document).ready(windowSize());
 $("div.burger-icon").click(burgerMenu);
 
@@ -31,8 +32,16 @@ function windowSize() {
         $(".contact div.temporary").removeClass("clear");
     }
 
+    if (tabletSize.matches) {
+        $('footer').addClass('hide');
+        $('#more').addClass('hide');
+    } else {
+        $('footer').removeClass('hide');
+        $('#more').removeClass('hide');
+    }
+
     var widthOfImg = $(".project img").width();
-    $("p.photo-text").css({"max-width": widthOfImg});
+    $("p.photo-text").css({"width": widthOfImg});
 
     var heightOfContent = $("header").outerHeight(true) + $("main").outerHeight(true) + $("footer").outerHeight(true);
 
@@ -50,5 +59,19 @@ function burgerMenu(){
     $("div.burger-icon, div.bar, #bar2, #bar1, #bar3, body").toggleClass("open");
 
 }
+
+var $div = $('div.project');
+var sortedDivs = $div.sort(function(a, b){
+    return ($(a).find(".project .container h2").text() < $(b).find(".project .container h2").text() ? 1 : -1);
+});
+$("#all-projects").html(sortedDivs);
+
+$('#sortBtn').on('click', function(){
+    var sortedDivs = $div.sort(function(a, b){
+        return ($(a).find(".project .container h2").text() < $(b).find(".project .container h2").text() ? 1 : -1);
+    });
+    $("#all-projects").html(sortedDivs);
+});
+
 
 window.onresize = windowSize;
