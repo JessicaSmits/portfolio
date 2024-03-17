e16a2d7d0699664f035a24b11a7a9ac68d80406e
ShinyUnicorn <70696290+JessicaSmits@users.noreply.github.com>
2024-03-17 18:52:53 +0100

    Update portfolio
    Added sorting system for my projects, overall cleaned up my code and i now use flexbox instead of float and em instead of px 😇.

--- js/main.js
+++ js/main.js
@@ -6,12 +6,24 @@ var mobileSize = window.matchMedia("(max-width: 719px)");
 var divHeight;
 var pHeight;
 var marginTop;
+var uniqueTags;
 
-$(function() {windowSize(); sortDivs()});
+
+$(function () { windowSize(); headerResizeOnScroll(); findTags(); filterButtons(); sortProjects()});
+
+window.onresize = windowSize;
+window.onscroll = headerResizeOnScroll;
+
+function headerResizeOnScroll() {
+    if ($(document).scrollTop() > 100) {
+        $("nav.standard li").css("font-size", "1em");
+    } else if ($(document).scrollTop() < 100) {
+        $("nav.standard li").css("font-size", "1.5em");
+    }
+}
 
 // buttons
 $("div.burger-icon").click(burgerMenu);
-$('#sortBtn').on('click', sortDivs);
 $('img').on('click', imgSize(this));
 
 function windowSize() {
@@ -19,7 +31,6 @@ function windowSize() {
     if (mobileSize.matches) {
         $("nav.standard").hide();
         $("nav.burger-menu, div.burger-icon").show();
-        $("section.contact div.links h3").hide();
         $('footer').hide();
         $('#more').hide();;
         $('div.overview div:last-child').addClass('bottom');
@@ -27,11 +38,10 @@ function windowSize() {
     } else {
         $("nav.standard").show();
         $("nav.burger-menu, div.burger-icon").hide();
-        $("section.contact div.links h3").show()
         $('footer').show();
         $('#more').show();
         $('div.overview div:last-child').removeClass('bottom');
-        
+
         // header auto height
         var navHeight = $('nav.standard').innerHeight();
         $('header').css({ 'height': navHeight });
@@ -56,7 +66,6 @@ function windowSize() {
         pHeight = $(this).find('p').height();
         marginTop = ((divHeight - pHeight) / 2);
 
-        console.log(marginTop);
         $(this).find('p').css({ 'marginTop': marginTop });
     });
 
@@ -70,22 +79,153 @@ function burgerMenu() {
 
 }
 
-// sort project from last to first
-function sortDivs() {
-    $('#all-projects div.project').removeClass('bottom');
+// automate alternate project rows
+function sortProjects() {
+    const projects = document.querySelectorAll('.project');
 
-    var $div = $('div.project');
-    var sortedDivs = $div.sort(function (a, b) {
-        return ($(a).find(".project .container h2").text() < $(b).find(".project .container h2").text() ? 1 : -1);
+    projects.forEach((project, index) => {
+        // Check if index is even or odd to determine flex-direction
+        if (index % 2 === 0) {
+        project.classList.add('left');
+        } else {
+        project.classList.add('right');
+        }
     });
-
-    $("#all-projects").html(sortedDivs);
-
-    $('#all-projects div.project:last-child').addClass('bottom');
 }
 
-function imgSize(clickedImg){
-    $(clickedImg).toggleClass("bigger-picture");
-} 
 
-window.onresize = windowSize;
+// filter projects on tags
+function findTags() {
+    // Getting all the tags from the projects
+    var tagsArray = $('.project .tag');
+    var tagsSet = new Set();
+
+    // Adding unique tags to the set
+    tagsArray.each(function () {
+        tagsSet.add($(this).text().trim());
+    });
+
+    // Converting the set back to an array
+    var uniqueTags = Array.from(tagsSet);
+
+    console.log(uniqueTags);
+
+    // Calling the function to create buttons based on unique tags
+    filterButtons(uniqueTags);
+}
+
+function filterButtons(tags) {
+    if (!Array.isArray(tags)) {
+        console.error("Tags is not an array");
+        return;
+    }
+    
+    $('#all-projects div.project').removeClass('bottom');
+
+    var buttonContainer = $('#button-container');
+    buttonContainer.empty(); // Clear any existing buttons
+
+    // Creating buttons for each unique tag
+    tags.forEach(function (tag) {
+        var button = $('<button/>', {
+            text: tag,
+            class: 'filter-button',
+            'data-filter': tag
+        });
+        buttonContainer.append(button);
+    });
+
+    var allButton = $('<button/>', { text: 'show all', class: 'filter-button', 'data-filter': 'all' });
+    buttonContainer.append(allButton);
+
+    var activeFilters = [];
+
+    // Adding click event to filter buttons
+    $('.filter-button').on('click', function () {
+        var filter = $(this).data('filter');
+
+        if ($(this).hasClass('active')) {
+            $(this).removeClass('active');
+            activeFilters = activeFilters.filter(function (item) {
+                return item !== filter;
+            });
+        } else {
+            $(this).addClass('active');
+            activeFilters.push(filter);
+        }
+
+        if (activeFilters.length === 0) {
+            $('.project').show();
+        } else {
+            $('.project').hide();
+            $('.project').each(function () {
+                var projectTags = $(this).find('.tag');
+                var showProject = activeFilters.every(function (filter) {
+                    return projectTags.hasClass(filter);
+                });
+                if (showProject) {
+                    $(this).show();
+                }
+            });
+        }
+
+        if (filter == 'all' ) {
+            $('.filter-button').removeClass('active');
+            $('.project').show();
+        }
+    });
+
+    $('#all-projects div.project:last-child').addClass('bottom');
+    sortProjects();
+    
+}
+
+// Call the findTags function to initiate the process
+findTags();
+
+
+
+function imgSize(clickedImg) {
+    $(clickedImg).toggleClass("bigger-picture");
+}
+
+// https://codepen.io/wuh/pen/RxvLoO
+$(function boxShadowCursor() {
+    $('h1').each(function () {
+        var el = $(this);
+        var elWidth = $(el).width();
+        var elHeight = $(el).height();
+        var elOffset = $(el).offset();
+        var viewportWidth = $(document).width();
+        var viewportHeight = $(document).height();
+        var elCentreX = elOffset.left + (elWidth / 2);
+        var elCentreY = elOffset.top + (elHeight / 2);
+        var elShadow = $(this).css('text-shadow');
+        var elShadowSplit = elShadow.split(' ');
+        var elShadowColor = [elShadowSplit[0], elShadowSplit[1], elShadowSplit[2]].join('');
+        var elShDpthX = parseInt(elShadowSplit[3].replace(/\D/g, ''));
+        var elShDpthY = parseInt(elShadowSplit[4].replace(/\D/g, ''));
+
+        $(window).on('mousemove', function (e) {
+            var offX = e.pageX - elCentreX;
+            var offY = e.pageY - elCentreY;
+
+            var newShDepthX = -Math.round(offX.map(0, viewportWidth / 2, 0, elShDpthX) * 7) + 2 + 'px';
+            var newShDepthY = -Math.round(offY.map(0, viewportHeight / 2, 0, elShDpthY) * 7) + 2 + 'px';
+            var newShadow = [newShDepthX, newShDepthY, elShadowSplit[5], elShadowColor];
+            $(el).css('text-shadow', newShadow.join(' '));
+
+            console.log(newShDepthX, newShDepthY)
+        });
+
+    });
+
+});
+
+Number.prototype.map = function (in_min, in_max, out_min, out_max) {
+    return (this - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
+
+
+
